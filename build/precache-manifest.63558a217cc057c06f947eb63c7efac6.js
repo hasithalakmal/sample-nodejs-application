398a416e96a79364b5e5323dc74918ce3f40d95a
hasithag <hasitha.gamage@sysco.com>
2020-02-15 09:56:14 +0530

    code cleanup

---
+++ build/precache-manifest.63558a217cc057c06f947eb63c7efac6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852aeb0a5c9879f7082a8893db722f99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c32d427246f856172d6e",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "babc12fabcf0f18d026e",
+    "url": "/static/css/main.fec45a73.chunk.css"
+  },
+  {
+    "revision": "c32d427246f856172d6e",
+    "url": "/static/js/2.b017adfe.chunk.js"
+  },
+  {
+    "revision": "2a7016412afc226bfd5ce83c577457f9",
+    "url": "/static/js/2.b017adfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "babc12fabcf0f18d026e",
+    "url": "/static/js/main.d0b3a675.chunk.js"
+  },
+  {
+    "revision": "f0446aadaf55b78d0bac",
+    "url": "/static/js/runtime-main.91df03ff.js"
+  }
+]);
--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
-import express from 'express';
-import request from 'request';
-import React from 'react';
-import {
-    renderToString
-} from 'react-dom/server';
-import App from './app';
-import template from './template';
+const express = require('express');
+const request = require('request');
+const path = require('path');
 
 const server = express();
+server.use(express.static(path.join(__dirname, '/app/')));
 
 var getAllStudents = function() {
     return new Promise(function(resolve, reject) {
@@ -27,21 +23,8 @@ var getAllStudents = function() {
 server.use('/assets', express.static('assets'));
 
 server.get('/', (req, res) => {
-            const isMobile = false;
-            const initialState = {
-                isMobile
-            };
-            const appString = renderToString( < App {
-                    ...initialState
-                }
-                />);
-
-                res.send(template({
-                    body: appString,
-                    title: 'Hello World from the server',
-                    initialState: JSON.stringify(initialState)
-                }));
-            });
+    res.sendFile(path.join(__dirname + '/app/index.html'));
+});
 
 server.get("/accounts", (req, res) => {
     var result = getAllStudents();
